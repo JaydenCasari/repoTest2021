de71b84242d50b87f3d6e7d8663e98ad52f9952f
Jayden Casari <jman1403@outlook.com>
2021-04-23 13:40:21 -0400

    Initial Commit

---
+++ repoTest2021/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  repoTest2021
+//
+//  Created by Jayden Casari on 4/23/21.
+//  Copyright © 2021 Jayden Casari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ repoTest2021Tests/repoTest2021Tests.swift
@@ -0,0 +1,34 @@
+//
+//  repoTest2021Tests.swift
+//  repoTest2021Tests
+//
+//  Created by Jayden Casari on 4/23/21.
+//  Copyright © 2021 Jayden Casari. All rights reserved.
+//
+
+import XCTest
+@testable import repoTest2021
+
+class repoTest2021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
